cdbf1a6cb3072c5f7178912f29a28f863ba1ded1
Modestas <61542037+ModestasZeringis@users.noreply.github.com>
2023-01-11 18:04:25 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+const handlebars = require('express3-handlebars')
+const express = require('express')
+
+module.exports = function(app){
+	app.engine('html', handlebars({ 
+		defaultLayout: 'main',
+		extname: ".html",
+		layoutsDir: __dirname + '/views/layouts'
+	}));
+
+
+	app.set('view engine', 'html');
+
+
+	app.set('views', __dirname + '/views');
+
+
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.urlencoded({ extended: true }))
+
+};
